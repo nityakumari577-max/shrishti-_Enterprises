c2b1411d12ec7ead4a99ebc5fe422583f74b2e4f
Nitya Kumari <nityakumari577@gmail.com>
2026-08-29 19:05:54 +0530

    Update backend and client

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express= require("express");
 const cors = require("cors");
-const dotenv = require("dotenv");const connectDB = require("./config/db");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
 dotenv.config();
 connectDB();
