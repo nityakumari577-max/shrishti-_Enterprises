a27cdf73f45ce14d2852faeff4f7f1b74cae32de
Nitya Kumari <nityakumari577@gmail.com>
2026-09-02 01:55:36 +0530

    Update navbar mobile styling

--- client/script.js
+++ client/script.js
@@ -3,4 +3,32 @@ const category = params.get("category");
 
 if (category) {
     document.getElementById("categoryTitle").innerText = category;
-}
+} 
+const currentPage =
+        window.location.pathname.split("/").pop() || "index.html";
+
+    const navLinks =
+        document.querySelectorAll("nav ul li a");
+
+    navLinks.forEach(link => {
+
+        const href = link.getAttribute("href");
+
+        // Home
+        if (
+            href === "#Home" &&
+            (currentPage === "" || currentPage === "index.html")
+        ) {
+            link.classList.add("active");
+        }
+
+        // Other pages
+        else if (
+            href &&
+            !href.startsWith("#") &&
+            href === currentPage
+        ) {
+            link.classList.add("active");
+        }
+
+    });
